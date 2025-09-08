fca5655e760064919afb499bc9ad2ee8716e8b32
nanjishidu <yijialiu212@gmail.com>
2025-09-08 17:02:03 +0800

    新增自定义词 和 子账号

--- src/Providers/JdtServiceProvider.php
+++ src/Providers/JdtServiceProvider.php
@@ -150,8 +150,6 @@ class JdtServiceProvider extends LaravelServiceProvider
     }
 
     /** 文本检测错误类型
-     * @param $method
-     * @param $text
      * @return array|mixed
      */
     public function wbjcGetCorrectAbility()
@@ -167,11 +165,12 @@ class JdtServiceProvider extends LaravelServiceProvider
     }
 
     /** 文本校对接口
-     * @param $method
-     * @param $text
+     * @param $text  文本内容
+     * @param $abilityIds  能力id
+     * @param $sonUserId  子账号Id
      * @return array|mixed
      */
-    public function wbjcArticleCorrectExternal($text, $abilityIds, $subKey = 0)
+    public function wbjcArticleCorrectExternal($text, $abilityIds, $sonUserId = '')
     {
         $uri = "/dataapp/api/umei/fw/open/wbjc/article_correct_external";
         if ($abilityIds == 'all') {
@@ -181,11 +180,8 @@ class JdtServiceProvider extends LaravelServiceProvider
             'text' => $text,
             'ability_ids' => $abilityIds,
         ];
-        if (!empty($subKey)) {
-            $sonUserId = config('jt.son_user.' . $subKey . 'son_user_id');
-            if (!empty($sonUserId)) {
-                $params['son_user_id'] = $sonUserId;
-            }
+        if (!empty($sonUserId)) {
+            $params['son_user_id'] = $sonUserId;
         }
         $resp = $this->httpPost($uri, $params, ['tranctionId' => md5(config('app_id') . time() . Str::random(4))]);
         if ($resp['code'] == 200) {
@@ -197,14 +193,288 @@ class JdtServiceProvider extends LaravelServiceProvider
     }
 
     /** 获取文本校对用户信息状态接口
-     * @param $method
-     * @param $text
      * @return array|mixed
      */
     public function wbjcGetWbjcAuthInfo()
     {
         $uri = "/dataapp/api/umei/fw/open/wbjc/get_wbjc_auth_info";
         $resp = $this->httpPost($uri);
+        if ($resp['code'] == 200) {
+            $code = '0000'; //获取成功
+        } else {
+            $code = '2000'; //获取失败
+        }
+        return $this->message($code, $resp['message'] ?? $resp['msg'], $resp['data'] ?? []);
+    }
+
+    /** 查询子账号
+     * @param $page 页码
+     * @param $pageCount 每页数量
+     * @param $searchValue 搜索值
+     * @return array|mixed
+     */
+    public function wbjcGetSonUser($page = 1, $pageCount = 10, $searchValue = '')
+    {
+        $uri = "/dataapp/api/umei/fw/open/wbjc/get_son_user";
+        $params = [
+            'page' => $page,
+            'pageCount' => $pageCount,
+        ];
+        if (!empty($searchValue)) {
+            $params['searchValue'] = $searchValue;
+        }
+        $resp = $this->httpPost($uri, $params);
+        if ($resp['code'] == 200) {
+            $code = '0000'; //获取成功
+        } else {
+            $code = '2000'; //获取失败
+        }
+        return $this->message($code, $resp['message'] ?? $resp['msg'], $resp['data'] ?? []);
+    }
+
+    /** 添加子账号
+     * @param $sonUserName  子账号名称
+     * @return array|mixed
+     */
+    public function wbjcAddSonUser($sonUserName)
+    {
+        $uri = "/dataapp/api/umei/fw/open/wbjc/add_son_user";
+        $resp = $this->httpPost($uri, [
+            'son_user_name' => $sonUserName,
+        ]);
+        if ($resp['code'] == 200) {
+            $code = '0000'; //获取成功
+        } else {
+            $code = '2000'; //获取失败
+        }
+        return $this->message($code, $resp['message'] ?? $resp['msg'], $resp['data'] ?? []);
+    }
+
+    /** 编辑子账号
+     * @param $updateSonUserId  子账号ID
+     * @param $sonUserName  子账号名称
+     * @return array|mixed
+     */
+    public function wbjcUpdateSonUser($updateSonUserId, $sonUserName)
+    {
+        $uri = "/dataapp/api/umei/fw/open/wbjc/update_son_user";
+        $resp = $this->httpPost($uri, [
+            'update_son_user_id' => $updateSonUserId,
+            'son_user_name' => $sonUserName,
+        ]);
+        if ($resp['code'] == 200) {
+            $code = '0000'; //获取成功
+        } else {
+            $code = '2000'; //获取失败
+        }
+        return $this->message($code, $resp['message'] ?? $resp['msg'], $resp['data'] ?? []);
+    }
+
+    /** 删除子账号
+     * @param $delSonUserId  子账号ID
+     * @return array|mixed
+     */
+    public function wbjcDelSonUser($delSonUserId)
+    {
+        $uri = "/dataapp/api/umei/fw/open/wbjc/del_son_user";
+        $resp = $this->httpPost($uri, [
+            'del_son_user_id' => $delSonUserId,
+        ]);
+        if ($resp['code'] == 200) {
+            $code = '0000'; //获取成功
+        } else {
+            $code = '2000'; //获取失败
+        }
+        return $this->message($code, $resp['message'] ?? $resp['msg'], $resp['data'] ?? []);
+    }
+
+    /** 获取自定义词列表
+     * @param $wordType  自定义词类型 1禁用词2敏感词3正词4错词5重点词
+     * @param $page  页码
+     * @param $pageCount  每页数量
+     * @param $searchWord  搜索词
+     * @param $sonUserId  子账号ID
+     * @return array|mixed
+     */
+    public function wbjcGetWordList($wordType, $page = 1, $pageCount = 10, $searchWord = '', $sonUserId = '')
+    {
+        $uri = "/dataapp/api/umei/fw/open/wbjc/get_word_list";
+        if (!in_array($wordType, [1, 2, 3, 4, 5])) {
+            return $this->message('2000', '自定义词类型错误');
+        }
+        $params = [
+            'word_type' => $wordType,
+            'page' => $page,
+            'totalPage' => $pageCount,
+        ];
+        if (!empty($searchWord)) {
+            $params['search_word'] = $searchWord;
+        }
+        if (!empty($sonUserId)) {
+            $params['son_user_id'] = $sonUserId;
+        }
+        $resp = $this->httpPost($uri, $params);
+        if ($resp['code'] == 200) {
+            $code = '0000'; //获取成功
+        } else {
+            $code = '2000'; //获取失败
+        }
+        return $this->message($code, $resp['message'] ?? $resp['msg'], $resp['data'] ?? []);
+    }
+
+    /** 添加自定义词
+     * @param $wordType  自定义词类型 1禁用词(word_reason:禁用的原因)2敏感词(word_reason:敏感类型)3正词4错词(word_reason:正词)5重点词
+     * @param $wordText  词名称
+     * @param $wordReason  词原因
+     * @param $sonUserId  子账号ID
+     * @return array|mixed
+     */
+    public function wbjcAddWord($wordType, $wordText, $wordReason = '', $sonUserId = '')
+    {
+        $uri = "/dataapp/api/umei/fw/open/wbjc/add_word";
+        if (!in_array($wordType, [1, 2, 3, 4, 5])) {
+            return $this->message('2000', '自定义词类型错误');
+        }
+        $params = [
+            'word_type' => $wordType,
+            'word_text' => $wordText,
+        ];
+        if (!empty($wordReason)) {
+            $params['word_reason'] = $wordReason;
+        }
+        if (!empty($sonUserId)) {
+            $params['son_user_id'] = $sonUserId;
+        }
+        $resp = $this->httpPost($uri, $params);
+        if ($resp['code'] == 200) {
+            $code = '0000'; //获取成功
+        } else {
+            $code = '2000'; //获取失败
+        }
+        return $this->message($code, $resp['message'] ?? $resp['msg'], $resp['data'] ?? []);
+    }
+
+    /** 编辑自定义词
+     * @param $detectionLexiconId  要操作的自定义词ID
+     * @param $wordText  词名称
+     * @param $wordReason  词原因
+     * @param $sonUserId  子账号ID
+     * @return array|mixed
+     */
+    public function wbjcEditWord($detectionLexiconId, $wordText, $wordReason = '', $sonUserId = '')
+    {
+        $uri = "/dataapp/api/umei/fw/open/wbjc/edit_word";
+        $params = [
+            'detection_lexicon_id' => $detectionLexiconId,
+            'word_text' => $wordText,
+        ];
+        if (!empty($wordReason)) {
+            $params['word_reason'] = $wordReason;
+        }
+        if (!empty($sonUserId)) {
+            $params['son_user_id'] = $sonUserId;
+        }
+        $resp = $this->httpPost($uri, $params);
+        if ($resp['code'] == 200) {
+            $code = '0000'; //获取成功
+        } else {
+            $code = '2000'; //获取失败
+        }
+        return $this->message($code, $resp['message'] ?? $resp['msg'], $resp['data'] ?? []);
+    }
+
+    /** 批量添加自定义词
+     * @param $words  自定义词库 格式：[['word_text' => 'a', 'word_reason' => 'b', 'word_type' => 1]]
+     * @param $sonUserId  子账号ID
+     * @return array|mixed
+     */
+    public function wbjcBatchAddWord($words = [], $sonUserId = '')
+    {
+        $uri = "/dataapp/api/umei/fw/open/wbjc/batch_add_word";
+        if (empty($words)) {
+            return $this->message('2000', '自定义词不允许为空');
+        }
+        $params['json_str'] = json_encode($words, JSON_UNESCAPED_UNICODE);
+        if (!empty($sonUserId)) {
+            $params['son_user_id'] = $sonUserId;
+        }
+        $resp = $this->httpPost($uri, $params);
+        if ($resp['code'] == 200) {
+            $code = '0000'; //获取成功
+        } else {
+            $code = '2000'; //获取失败
+        }
+        return $this->message($code, $resp['message'] ?? $resp['msg'], $resp['data'] ?? []);
+    }
+
+    /** 通用-词操作状态
+     * @param $detectionLexiconIds  词库ID，多个逗号分隔，支持数组
+     * @param $status  状态1启用2停用
+     * @param $sonUserId  子账号ID
+     * @return array|mixed
+     */
+    public function wbjcHandleWordStatus($detectionLexiconIds, $status = 1, $sonUserId = '')
+    {
+        $this->wbjcHandleWord(2, $detectionLexiconIds, $status, $sonUserId);
+    }
+
+    /** 通用-词操作删除
+     * @param $detectionLexiconIds  词库ID，多个逗号分隔，支持数组
+     * @param $sonUserId  子账号ID
+     * @return array|mixed
+     */
+    public function wbjcDelWord($detectionLexiconIds, $sonUserId = '')
+    {
+        $this->wbjcHandleWord(3, $detectionLexiconIds, 1, $sonUserId);
+    }
+
+    /** 通用-词操作
+     * @param $handleType  操作类型 2 启用停用 3 删除
+     * @param $detectionLexiconIds  词库ID，多个逗号分隔，支持数组
+     * @param $status  状态1启用2停用
+     * @param $sonUserId  子账号ID
+     * @return array|mixed
+     */
+    public function wbjcHandleWord($handleType, $detectionLexiconIds, $status = 1, $sonUserId = '')
+    {
+        $uri = "/dataapp/api/umei/fw/open/wbjc/handle_word";
+        $params = [
+            'handle_type' => $handleType,
+            'detection_lexicon_ids' => is_array($detectionLexiconIds) ? implode(',', $detectionLexiconIds) : $detectionLexiconIds,
+        ];
+        if ($handleType == 2) {
+            $params['status'] = $status;
+        }
+        if (!empty($sonUserId)) {
+            $params['son_user_id'] = $sonUserId;
+        }
+        $resp = $this->httpPost($uri, $params);
+        if ($resp['code'] == 200) {
+            $code = '0000'; //获取成功
+        } else {
+            $code = '2000'; //获取失败
+        }
+        return $this->message($code, $resp['message'] ?? $resp['msg'], $resp['data'] ?? []);
+    }
+
+    /**  通用-清空词
+     * @param $wordType  自定义词类型 1禁用词2敏感词3正词4错词5重点词6自定义领导人职务7自定义领导人排序
+     * @param $sonUserId  子账号ID
+     * @return array|mixed
+     */
+    public function wbjcDelAllWord($wordType, $sonUserId = '')
+    {
+        $uri = "/dataapp/api/umei/fw/open/wbjc/del_all_word";
+        if (!in_array($wordType, [1, 2, 3, 4, 5, 6, 7])) {
+            return $this->message('2000', '自定义词类型错误');
+        }
+        $params = [
+            'word_type' => $wordType,
+        ];
+        if (!empty($sonUserId)) {
+            $params['son_user_id'] = $sonUserId;
+        }
+        $resp = $this->httpPost($uri, $params);
         if ($resp['code'] == 200) {
             $code = '0000'; //获取成功
         } else {
